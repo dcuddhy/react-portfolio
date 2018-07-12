cada50960f58ea94c301f05acd776f1a76936ec4
dcuddhy <dcuddhy@gmail.com>
2018-07-11 21:28:22 -0600

    Added portfolio images to modals.  Changed default text color to dark blue

--- src/components/portfoliomodals.js
+++ src/components/portfoliomodals.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-
+import PortfolioTBCDC from "../assets/images/screen-tbcdc.png";
+import PortfoliogCamp from "../assets/images/screen-gcamp.png";
+import PortfolioListen from "../assets/images/screen-listen.png";
+import PortfolioFolio from "../assets/images/screen-folio.png";
+import PortfolioDCP from "../assets/images/screen-dcp.png";
+import PortfolioNotes from "../assets/images/screen-notes-combo.png";
 
 class PortfolioModals extends Component {
     render() {
@@ -17,9 +22,9 @@ class PortfolioModals extends Component {
                         <div className="row">
                             <div className="col-lg-8 col-lg-offset-2">
                                 <div className="modal-body">
-                                    <h2>Project Title</h2>
+                                    <h2>The Beard Club</h2>
                                     <hr className="star-primary" />
-                                    <img src="img/portfolio/cabin.png" className="img-responsive img-centered" alt="" />
+                                    <img src={PortfolioTBCDC} className="img-responsive img-centered" alt="" />
                                     <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
                                     <ul className="list-inline item-details">
                                         <li>Client:
@@ -54,9 +59,9 @@ class PortfolioModals extends Component {
                         <div className="row">
                             <div className="col-lg-8 col-lg-offset-2">
                                 <div className="modal-body">
-                                    <h2>Project Title</h2>
+                                    <h2>gCamp</h2>
                                     <hr className="star-primary" />
-                                    <img src="img/portfolio/cake.png" className="img-responsive img-centered" alt="" />
+                                    <img src={PortfoliogCamp} className="img-responsive img-centered" alt="" />
                                     <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
                                     <ul className="list-inline item-details">
                                         <li>Client:
@@ -91,9 +96,9 @@ class PortfolioModals extends Component {
                         <div className="row">
                             <div className="col-lg-8 col-lg-offset-2">
                                 <div className="modal-body">
-                                    <h2>Project Title</h2>
+                                    <h2>Listen</h2>
                                     <hr className="star-primary" />
-                                    <img src="img/portfolio/circus.png" className="img-responsive img-centered" alt="" />
+                                    <img src={PortfolioListen} className="img-responsive img-centered" alt="" />
                                     <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
                                     <ul className="list-inline item-details">
                                         <li>Client:
@@ -128,9 +133,9 @@ class PortfolioModals extends Component {
                         <div className="row">
                             <div className="col-lg-8 col-lg-offset-2">
                                 <div className="modal-body">
-                                    <h2>Project Title</h2>
+                                    <h2>Portfolio 2015</h2>
                                     <hr className="star-primary" />
-                                    <img src="img/portfolio/game.png" className="img-responsive img-centered" alt="" />
+                                    <img src={PortfolioFolio} className="img-responsive img-centered" alt="" />
                                     <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
                                     <ul className="list-inline item-details">
                                         <li>Client:
@@ -165,9 +170,9 @@ class PortfolioModals extends Component {
                         <div className="row">
                             <div className="col-lg-8 col-lg-offset-2">
                                 <div className="modal-body">
-                                    <h2>Project Title</h2>
+                                    <h2>Drew Cuddhy Photography</h2>
                                     <hr className="star-primary" />
-                                    <img src="img/portfolio/safe.png" className="img-responsive img-centered" alt="" />
+                                    <img src={PortfolioDCP} className="img-responsive img-centered" alt="" />
                                     <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
                                     <ul className="list-inline item-details">
                                         <li>Client:
@@ -202,9 +207,9 @@ class PortfolioModals extends Component {
                         <div className="row">
                             <div className="col-lg-8 col-lg-offset-2">
                                 <div className="modal-body">
-                                    <h2>Project Title</h2>
+                                    <h2>Local Notes</h2>
                                     <hr className="star-primary" />
-                                    <img src="img/portfolio/submarine.png" className="img-responsive img-centered" alt="" />
+                                    <img src={PortfolioNotes} className="img-responsive img-centered" alt="" />
                                     <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
                                     <ul className="list-inline item-details">
                                         <li>Client:
